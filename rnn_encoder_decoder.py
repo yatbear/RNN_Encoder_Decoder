2ab8a47af3dae605e33d5234276e10dea579bac5
yatjing <jingi1024@gmail.com>
2015-12-10 03:25:06 -0500

    fixed some major bugs, max pooling is not working yet

--- rnn_encoder_decoder.py
+++ rnn_encoder_decoder.py
@@ -6,9 +6,13 @@
 #
 # Author: yatbear <sapphirejyt@gmail.com>
 #         2015-11-19: Created for MT independent study.
+#         2015-12-10: Fixed major bugs.
 
+import theano
 import theano.tensor as T
+import theano.tensor.signal.downsample as ds
 import numpy as np
+from collections import OrderedDict
 
 class rnn_encoder_decoder(object):
 
@@ -19,146 +23,173 @@ class rnn_encoder_decoder(object):
         # ny :: target vocabulary size
 
         # Parameters of the RNN encoder
-        self.emb = T.shared(name='embeddings',
-                            value=0.2 * np.random.uniform(-1.0, 1.0, (nx, ne))
-                            .astype(T.config.floatX))
-        self.Wx = T.shared(name='Wx',
-                            value=0.2 * np.random.uniform(-1.0, 1.0, (nh, ne))
-                            .astype(T.config.floatX))
-        self.Wh_e = T.shared(name='Wh_e',
-                            value=0.2 * np.random.uniform(-1.0, 1.0, (nh, nh))
-                            .astype(T.config.floatX))
-        self.V_e = T.shared(name='V_e',
-                            value=0.2 * np.random.uniform(-1.0, 1.0, (nh, nh))
-                            .astype(T.config.floatX))
-        self.h_e_0 = T.shared(name='h_e_0',
-                            value=np.zeros(nh,
-                            dtype=T.config.floatX))
+        self.emb = theano.shared(name='embeddings',
+                                value=0.2 * np.random.uniform(-1.0, 1.0, (nx, ne))
+                                .astype(theano.config.floatX))
+        self.Wx = theano.shared(name='Wx',
+                                value=0.2 * np.random.uniform(-1.0, 1.0, (nh, ne))
+                                .astype(theano.config.floatX))
+        self.Wh_e = theano.shared(name='Wh_e',
+                                value=0.2 * np.random.uniform(-1.0, 1.0, (nh, nh))
+                                .astype(theano.config.floatX))
+        self.V_e = theano.shared(name='V_e',
+                                value=0.2 * np.random.uniform(-1.0, 1.0, (nh, nh))
+                                .astype(theano.config.floatX))
+        self.h_e_0 = theano.shared(name='h_e_0',
+                                value=np.zeros(nh, 
+                                dtype=theano.config.floatX))
 
         # Parameters of the RNN decoder
-        self.V_d = T.shared(name='V_d',
-                            value=0.2 * np.random.uniform(-1.0, 1.0, (nh, nh))
-                            .astype(T.config.floatX))
-        self.Wc = T.shared(name='Wc',
-                            value=0.2 * np.random.uniform(-1.0, 1.0, (nh, nh))
-                            .astype(T.config.floatX))
-        self.Wh_d = T.shared(name='Wh_d',
-                            value=0.2 * np.random.uniform(-1.0, 1.0, (nh, nh))
-                            .astype(T.config.floatX))
-        self.Wy = T.shared(name='Wy',
-                            value=0.2 * np.random.uniform(-1.0, 1.0, (nh, ne))
-                            .astype(T.config.floatX))
-        self.y_0 = T.shared(name='y_0',
-                            value=np.zeros(ne,
-                            dtype=T.config.floatX))
+        self.V_d = theano.shared(name='V_d',
+                                value=0.2 * np.random.uniform(-1.0, 1.0, (nh, nh))
+                                .astype(theano.config.floatX))
+        self.Wc = theano.shared(name='Wc',
+                                value=0.2 * np.random.uniform(-1.0, 1.0, (nh, nh))
+                                .astype(theano.config.floatX))
+        self.Wh_d = theano.shared(name='Wh_d',
+                                value=0.2 * np.random.uniform(-1.0, 1.0, (nh, nh))
+                                .astype(theano.config.floatX))
+        self.Wy = theano.shared(name='Wy',
+                                value=0.2 * np.random.uniform(-1.0, 1.0, (ny, nh))
+                                .astype(theano.config.floatX))
+        self.y_0 = theano.shared(name='y_0',
+                                value=np.zeros(ny,
+                                dtype=theano.config.floatX))
 
         # Parameters of the output layer
-        self.Oh = T.shared(name='Oh',
-                            value=0.2 * np.random.uniform(-1.0, 1.0, (2*nh, nh))
-                            .astype(T.config.floatX))
-        self.Oy = T.shared(name='Oy',
-                            value=0.2 * np.random.uniform(-1.0, 1.0, (2*nh, ny))
-                            .astype(T.config.floatX))
-        self.Oc = T.shared(name='Oc',
-                            value=0.2 * np.random.uniform(-1.0, 1.0, (2*nh, nh))
-                            .astype(T.config.floatX))
-        self.Gl = T.shared(name='Gl',
-                            value=0.2 * np.random.uniform(-1.0, 1.0, (ny, ne))
-                            .astype(T.config.floatX))
-        self.Gr = T.shared(name='Gr',
-                            value=0.2 * np.random.uniform(-1.0, 1.0, (ne, nh))
-                            .astype(T.config.floatX))
+        self.Oh = theano.shared(name='Oh',
+                                value=0.2 * np.random.uniform(-1.0, 1.0, (nh, nh))
+                                .astype(theano.config.floatX))
+        self.Oy = theano.shared(name='Oy',
+                                value=0.2 * np.random.uniform(-1.0, 1.0, (nh, ny))
+                                .astype(theano.config.floatX))
+        self.Oc = theano.shared(name='Oc',
+                                value=0.2 * np.random.uniform(-1.0, 1.0, (nh, nh))
+                                .astype(theano.config.floatX))
+        ## For max pooling computation
+        # self.Oh = theano.shared(name='Oh',
+        #                         value=0.2 * np.random.uniform(-1.0, 1.0, (2*nh, nh))
+        #                         .astype(theano.config.floatX))
+        # self.Oy = theano.shared(name='Oy',
+        #                         value=0.2 * np.random.uniform(-1.0, 1.0, (2*nh, ny))
+        #                         .astype(theano.config.floatX))
+        # self.Oc = theano.shared(name='Oc',
+        #                         value=0.2 * np.random.uniform(-1.0, 1.0, (2*nh, nh))
+        #                         .astype(theano.config.floatX))
+        self.Gl = theano.shared(name='Gl',
+                                value=0.2 * np.random.uniform(-1.0, 1.0, (ny, ne))
+                                .astype(theano.config.floatX))
+        self.Gr = theano.shared(name='Gr',
+                                value=0.2 * np.random.uniform(-1.0, 1.0, (ne, nh))
+                                .astype(theano.config.floatX))
 
         # Bundle
         self.params = [self.emb, self.Wx, self.Wh_e, self.V_e, 
             self.V_d, self.Wh_d, self.Wy, self.Oh, self.Oy, self.Oc, self.Gl, self.Gr]
 
-    # Encode an input phrase into a summary vector
-    def encode(x):
-        # Construct encoder recursion 
-        def en_recurrence(x_t, h_e_tm1):
-            h_e_t = T.tanh(T.dot(self.Wx, x_t) 
-                        + T.dot(self.Wh_e, h_e_tm1))
-            return h_e_t
+    
+    def encoder_decoder_init(self, x_seq, y_seq, lr):
+        # x_seq :: source phrase as one-hot-vectors
+        # y_seq :: target phrase as one-hot-vectors
+        #    lr :: learning rate
 
-        # Compute the encoder hidden state recursively
-        h_e, _ = T.scan(fn=en_recurrence, 
-                        sequence=x, 
-                        outputs_info=self.h_e_0,
-                        n_steps=x.shape[0])
+        # Encode an input phrase into a summary vector
+        def encode(x_seq):   
+            # Build input from embedding matrix
+            x = T.dot(x_seq, self.emb)
 
-        # Compute the summary vector
-        c = T.tanh(T.dot(self.V_e, h_e[-1]))
-        return c
+            # Construct encoder recursion 
+            def en_recurrence(x_t, h_e_tm1):
+                h_e_t = T.tanh(T.dot(self.Wx, x_t) 
+                            + T.dot(self.Wh_e, h_e_tm1))
+                return h_e_t
 
-    # Decode the summary vector into a target sequence
-    def decode(c, y):
-        # Initialize the decoder hidden state
-        h_d_0 = T.tanh(T.dot(self.V_d, c))
+            # Compute the encoder hidden state recursively
+            h_e, _ = theano.scan(fn=en_recurrence, 
+                                sequences=x, 
+                                outputs_info=self.h_e_0,
+                                n_steps=x.shape[0])
 
-        # Construct decoder recursion
-        def de_recurrence(h_d_tm1, c, y_t):
-            # Compute hidden layer
-            h_t = T.tanh(T.dot(self.Wc, c) 
-                        + T.dot(self.Wh_d, h_d_tm1)
-                        + T.dot(self.Wy, y_t))
+            # Compute the summary vector
+            c = T.tanh(T.dot(self.V_e, h_e[-1]))
+            return c
+
+        # Decode the summary vector into a target sequence
+        def decode(c, y_seq):
+            # Initialize the decoder hidden state
+            self.h_d_0 = T.tanh(T.dot(self.V_d, c))
+
+            # Construct decoder recursion
+            def de_recurrence(t, c, y_seq):
+                y_t = y_seq[t]
+                y_tm1 = y_seq[t-1] if t == 0 else self.y_0
+
+                # Compute hidden layer
+                h_d_t = T.tanh(T.dot(self.Wc, c) 
+                            + T.dot(self.Wh_d, self.h_d_0)
+                            + T.dot(y_t, self.Wy))
+
+                self.h_d_0 = h_d_t[-1]
             
-            # Compute output layer
-            ss_t = T.dot(self.Oh, h_t) + T.dot(self.Oy, y_tm1) + T.dot(self.Oc, c)
+                # Compute output layer
+                ss_t = T.dot(self.Oh, h_d_t) + T.dot(self.Oy, y_tm1) + T.dot(self.Oc, c)
 
-            # Compute maxout units
-            for i in xrange(nh):
-                s_t[i] = T.max(ss_t[2*i:2*(i+1)])
+                # Compute maxout units
+                # s_t = ds.max_pool_2d(ss_t, (1, 2), ignore_border=True) 
 
-            # Compute probability of generating the target phrase
-            G = T.dot(self.Gl, self.Gr)
-            p_t = T.nnet.softmax(T.dot(G, s_t))
+                # Compute probability of generating the target phrase
+                G = T.dot(self.Gl, self.Gr)
+                p_t = T.nnet.softmax(T.flatten(T.dot(G, ss_t)))
 
-            # Compute the negative log-likelihood
-            nll_t = -T.log(p_t)
-            return [h_t, nll_t]
+                # Compute the negative log-likelihood
+                nll_t = -T.log(p_t)
+                return nll_t
 
-        [h, nll], _ = T.scan(fn=de_recurrence,
-                            outputs_info=[h_d_0, None],
-                            non_sequences=[c, self.y_0],
-                            n_steps=y.shape[0])
-        return T.mean(nll)
-
-    def train(self):
-        # Build input from embedding matrix
-        idxs = T.imatrix()
-        x = self.emb[idxs].reshape((idxs.shape[0], ne))
-        y_seq = T.ivector('y_sequence') # labels
+            nll, _ = theano.scan(fn=de_recurrence,
+                                    sequences=T.arange(y_seq.shape[0]),
+                                    outputs_info=None,
+                                    non_sequences=[c, y_seq],
+                                    n_steps=y_seq.shape[0])
+            return T.mean(nll)
 
         # Get the summary vector
-        c = encode(x)
+        c = encode(x_seq)
+
         # Get the negative log-likelihood
         seq_nll = decode(c, y_seq) 
+        # print seq_nll.eval()
 
         # Compute all the gradients automatically to maximize the log-likelihood  
-        lr = T.scalar('lr')
+        # lr = T.scalar('lr')   
         seq_gradients = T.grad(seq_nll, self.params)
         seq_updates = OrderedDict((p, p - lr*g)
                                     for p, g in zip(self.params, seq_gradients))
-        self.classify = theano.function(inputs=[idxs, y_seq, lr],
+
+        # print seq_gradients.eval()
+        X = T.fmatrix('X')
+        Y = T.fmatrix('Y')
+
+        self.train_pair = theano.function(inputs=[X, Y],
                                         outputs=seq_nll,
+                                        on_unused_input='ignore',
+                                        allow_input_downcast=True,
                                         updates=seq_updates)
+        self.score_pair = theano.function(inputs=[X, Y], 
+                                        on_unused_input='ignore', 
+                                        allow_input_downcast=True, 
+                                        outputs=seq_nll)
+     
+    def train(self, x_seq, y_seq):
+        nll = self.train_pair(x_seq, y_seq)
+        return nll
 
-    def test(self):
-        # Build input from embedding matrix
-        idxs = T.imatrix()
-        x = self.emb[idxs].reshape((idxs.shape[0], ne))
-        y_seq = T.ivector('y_sequence') # labels
-
-        # Get the summary vector
-        c = encode(x)
-        # Get the negative log-likelihood
-        seq_nll = decode(c, y_seq) 
-        return seq_nll # use as rescoring feature
+    def score(self, x_seq, y_seq):
+        nll = self.score_pair(x_seq, y_seq)
+        return nll
 
 def main():
-    # Read input
-    phrase_table = [line for line in open('phrase-table').readlines()]
+    # Read input 
+    phrase_table = [line for line in open('phrase-table').readlines()][9217377:9217477]
     x_phrases = [line[0].strip().split() for line in phrase_table]
     y_phrases = [line[1].strip().split() for line in phrase_table]
 
@@ -186,11 +217,21 @@ def main():
 
     for phrase in y_phrases:
         # Construct training labels as one-hot-vectors
-        y = np.zeros((len(y_phrases), ny)) 
+        y = np.zeros((len(y_phrases), ny))
         for (i, word) in enumerate(phrase):
             j = y_vcb.index(word) if word in y_vcb else ny-1
             y[i][j] = 1.0
         Y.append(y) 
+
+    rnn = rnn_encoder_decoder(nx, ny)
+    lr = theano.shared(0.01)
+    for i, (x, y) in enumerate(zip(X, Y)):
+        rnn.encoder_decoder_init(x, y, lr)
+        score = rnn.train(x, y)
+        print phrase_table[i][0], phrase_table[i][1], phrase_table[i][2], score
+    # for i, (x, y) in enumerate(zip(X, Y)):
+    #     score = rnn.score(x, y)
+    #     print phrase_table[i][0], phrase_table[i][1], phrase_table[i][2], score
 
 if __name__ == '__main__':
     main()
